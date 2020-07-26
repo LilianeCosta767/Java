bb1ac97b5126e196691edfb5c6b72fb30820db63
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-07-26 01:09:59 -0300

    exercicio de polimorfismo de sobrecarga

---
+++ aula13Java/src/aula13java/Aula13Java.java
@@ -0,0 +1,10 @@
+package aula13java;
+public class Aula13Java {
+    public static void main(String[] args) {
+        Cachorro c = new Cachorro();
+        
+        c.reagir(2, 12.5f);
+        c.reagir(17, 4.5f);
+    }
+    
+}
---
+++ aula13Java/src/aula13java/Mamifero.java
@@ -0,0 +1,11 @@
+package aula13java;
+public class Mamifero extends Animal{
+    protected String corPelo;
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Som de Mamífero");
+    }
+    
+    
+}
