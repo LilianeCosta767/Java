2926bbdbcb859d9b418dbffd89e5fbc6eee93cca
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-08-20 13:15:18 -0300

    for decrescente

---
+++ OrdemDecrescente/src/ordemdecrescente/OrdemDecrescente.java
@@ -0,0 +1,14 @@
+package ordemdecrescente;
+
+import java.util.Scanner;
+
+public class OrdemDecrescente {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        for(int i = num; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+    
+}
